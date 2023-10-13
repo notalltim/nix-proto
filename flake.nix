653813bec94058d8d739e6018ee791c07e2f07ab
Timothy Gallion <tgallion@areai.com>
2023-10-12 20:50:09 -0400

    remove flake utils

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     nix-std.url = "github:chessai/nix-std";
     nix-filter.url = "github:numtide/nix-filter";
   };
-  outputs = { self, nixpkgs, flake-utils, nix-std, nix-filter, ... }@inputs:
+  outputs = { self, nixpkgs, nix-std, nix-filter, ... }@inputs:
     let
       std = nix-std.lib;
       lib = nixpkgs.lib;
