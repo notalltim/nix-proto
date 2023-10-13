2609970a1d3d9e1d34b12ecd8124a2ed218c2178
Timothy Gallion <tgallion@areai.com>
2023-10-12 20:45:09 -0400

    potential import fixes for python

--- python/default.nix
+++ python/default.nix
@@ -13,18 +13,20 @@
 
     buildDeps = [ python310Packages.protobuf ] ++ (toBuildDepsPy meta.protoDeps pkgs) ++ buildInputs;
     nativeBuildInputs = [ protobuf python310Packages.setuptools ] ++ buildDeps; # for import checking
-    propagateBuildInputs = buildDeps;
+    propagatedBuildInputs = buildDeps;
     doCheck = false;
 
     format = "pyproject";
+    pyproject = true;
     dependencies = inputDependencies ++ [ "protobuf" ] ++ toPythonDependencies meta.protoDeps;
-    py_project_toml = toPyProjectTOML { inherit name; inherit version; inherit dependencies; };
+    py_project_toml = toPyProjectTOML { inherit name; inherit version; inherit dependencies; meta_name = meta.name; };
 
     prePatch = ''
       mkdir -p src/${name}
     '';
     patchPhase = inputPatchPhase;
     postPatch = ''
+      for d in src/**/*/; do touch -- "$d/__init__.py"; done
       cat > pyproject.toml << EOF ${py_project_toml}
       EOF
     '';
@@ -43,18 +45,22 @@
           protoc ${(proto_lib.toProtocInclude (meta.protoDeps ++ [ meta ]))} --python_out=src/${meta.name + suffix} $proto
           echo  "from .$(realpath --relative-to="${meta.src}" "$proto" | sed 's/\.[^.]*$//;s/[/]/./g;s/*[.]\././')_pb2 import *" >> src/${meta.name + suffix}/__init__.py
         done
+        path=$(realpath --relative-to="${meta.src}" "$proto" | sed 's/\.[^.]*$//;s/[/]/./g;s/*[.]\././;s/\.[^.]*$//')
+        for py in `find "./src/${meta.name + suffix}" -type f -name "*_pb2.py"`; do
+          sed -i "s/from\ $path/from\ ./g" $py
+        done
         runHook postPatch
       '';
     inputDependencies = [ ];
     buildInputs = [ ];
   };
 
-  grpc_package = { python310Packages, protobuf, pkgs, grpc }: package rec {
+  grpc_package = { python310Packages, protobuf, pkgs, grpc, openssl, zlib, stdenv }: package rec {
     inherit python310Packages;
     inherit protobuf;
     inherit pkgs;
     suffix = "_grpc_py";
-    buildInputs = [ grpc python310Packages.grpcio python310Packages.grpcio-tools ];
+    buildInputs = [ python310Packages.grpcio python310Packages.grpcio-tools grpc openssl zlib stdenv ] ++ (toBuildDepsPy [ meta ] pkgs);
     inputDependencies = [ "grpcio" ];
     inputPatchPhase =
       ''
@@ -62,7 +68,11 @@
         shopt -s globstar
         for proto in `find "${meta.src}" -type f -name "*.proto"`; do
           python -m grpc_tools.protoc ${ (proto_lib.toProtocInclude (meta.protoDeps ++ [ meta ]))} --grpc_python_out=src/${meta.name + suffix} $proto
-          echo  "from .$(realpath --relative-to="${meta.src}" "$proto" | sed 's/\.[^.]*$//;s/[/]/./g;s/*[.]\././')_grpc_pb2 import *" >> src/${meta.name + suffix}/__init__.py
+          echo  "from .$(realpath --relative-to="${meta.src}" "$proto" | sed 's/\.[^.]*$//;s/[/]/./g;s/*[.]\././')_pb2_grpc import *" >> src/${meta.name + suffix}/__init__.py
+        done
+        path=$(realpath --relative-to="${meta.src}" "$proto" | sed 's/\.[^.]*$//;s/[/]/./g;s/*[.]\././;s/\.[^.]*$//')
+        for py in `find "./src/${meta.name + suffix}" -type f -name "*_pb2_grpc.py"`; do
+          sed -i "s/from\ $path/from\ ${meta.name}_proto_py.$path/g" $py
         done
         runHook postPatch
       '';
